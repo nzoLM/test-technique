8c8db81fb3484e7e48c1a5aa3982f956c4eaaa8e
Enzo <enzo.gouge@epitech.eu>
2025-05-10 17:53:44 +0200

    feat: add input and start to implement multiselect

--- app/test-technique/page.tsx
+++ app/test-technique/page.tsx
@@ -1,13 +1,30 @@
-'use client'
-import { Button } from "@/components/ui/button"
-import questions from "@/data/questions.json"
-import { useState } from "react"
+'use client';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+import questions from "@/data/questions.json";
+import { useState } from "react";
+
 export default function TestTechnique() {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const handleNext = () => {
     if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion((prev) => prev + 1)
+      setCurrentQuestion((prev) => prev + 1);
+    }
+  }
+  const handlePrevious = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion((prev) => prev - 1);
     }
   }
 
@@ -16,7 +33,47 @@ export default function TestTechnique() {
   return (
     <div className="flex h-screen w-full items-center justify-center p-4">
       <div className="max-w-md w-full space-y-4">
-        
+
+        <div key={question.id} className="flex flex-col gap-2">
+          <label htmlFor={question.id}>{question.title}</label>
+          {question.type === "text" && <Input id={question.id} type="text" placeholder="Name..."></Input>}
+          {question.type === "number" && <Input id={question.id} type="number" placeholder="Age"></Input>}
+          {question.type === "radio" && (
+            <RadioGroup id={question.id}>
+              {question.options?.map((value) => (
+                <div key={value} className="space-x-2">
+                  <RadioGroupItem value={value} id={value}>{value}</RadioGroupItem>
+                  <label htmlFor={value}>{value}</label>
+                </div>
+              ))}
+            </RadioGroup>)}
+          {
+            question.type === "multiselect" && (
+                <select name="hobbies" id={question.id} multiple>
+                  {question.options?.map((value)=>(
+                    <option key={value} value={value}>{value}</option>
+                  ))}
+                </select>
+                )
+          }
+          {
+            question.type === "textarea" && (
+              <Textarea name="about" id={question.id} cols={33} placeholder={question.description}></Textarea>
+            )
+          }
+        </div>
+        <div className="flex justify-between">
+          {currentQuestion > 0 && 
+                  <Button onClick={handlePrevious}>Previous</Button>
+                  }
+          {currentQuestion < questions.length - 1 && 
+                  <Button className="self-start" onClick={handleNext}>Next</Button>
+                  }
+          {currentQuestion === questions.length - 1 && 
+                  <Button type="submit">Submit</Button>
+                  }
+        </div>
+
       </div>
     </div>
   )
